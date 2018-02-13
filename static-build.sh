acfdfbe7872c483be2cd9f213d901df15508036a
Jake Bailey <jacob.b.bailey@gmail.com>
2018-02-13 01:31:21 -0600

    stdin instead of volume mounting

--- static-build.sh
+++ static-build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 
+tar -x -C /go/src
+
 # $@ is intentionally not wrapped in quotes.
 go install -installsuffix static -ldflags "-s -w -extldflags '-static' $GO_LDFLAGS" $@
 
 cd /go/bin
-tar -cf - *
+tar -c *
